a66e24335bd1e401075c5a03c0063793e0343b73
Alok Kumar Singh <alok18121991@gmail.com>
2024-02-21 21:18:56 +0530

    added https support on prod env

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"alok/web-service-budget/configs"
 	"alok/web-service-budget/routes"
+	"fmt"
+	"net/http"
+
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -11,13 +15,32 @@ import (
 func main() {
 
 	e := echo.New()
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*"},
-	}))
+	APP_ENV := "APP_ENV"
+	if getEnv(APP_ENV) == "prod" {
+		e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+			AllowOrigins: []string{"*"},
+		}))
+	} else {
+		e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+			AllowOrigins: []string{"*"},
+		}))
+	}
 
 	routes.UserRoute(e)
 	routes.ExpenseRoute(e)
 	configs.ConnectMongoDB()
 
-	e.Logger.Fatal(e.Start(":8080"))
+	fmt.Println("app env :", getEnv(APP_ENV))
+
+	if getEnv(APP_ENV) == "prod" {
+		if err := e.StartTLS(":8080", getEnv("APP_SERVER_CERT"), getEnv("APP_SERVER_KEY")); err != http.ErrServerClosed {
+			e.Logger.Fatal(err)
+		}
+	} else {
+		e.Logger.Fatal(e.Start(":8080"))
+	}
+}
+
+func getEnv(property string) string {
+	return os.Getenv(property)
 }
